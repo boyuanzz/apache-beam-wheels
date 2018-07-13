36f1f0f482d8a6d0fb5a3e2c4fde39c63db69c64
Boyuan Zhang <boyuanz@google.com>
2018-07-13 15:11:12 -0700

    Modify scripts again

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,16 +1,11 @@
 # Define custom utilities for deploying on travis
 
 function deploy {
-  printenv
-  cd ${TRAVIS_BUILD_DIR}/wheelhouse
-  mkdir test_wheels 
-  for artifact in *.*; do
+  svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0/test_wheels
+  cd test_wheels
+  for artifact in ../*.*; do
     echo $artifact
-    mv $artifact test_wheels/$artifact;
+    svn add $artifact
   done
-  svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0
-  cd 2.4.0
-  mv ../test_wheels .
-  svn add test_wheels
   svn commit --non-interactive --no-auth-cache --username $WHEELHOUSE_UPLOADER_USERNAME --password $WHEELHOUSE_UPLOADER_SECRET -m "Upload python wheels"
 }
