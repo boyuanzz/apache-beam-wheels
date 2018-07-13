8fb966fbb15636c43a00b393678a78c18ad73969
Boyuan Zhang <boyuanz@google.com>
2018-07-13 15:25:54 -0700

    Modify artifacts copy path

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,11 +1,13 @@
 # Define custom utilities for deploying on travis
 
 function deploy {
+  cd {TRAVIS_BUILD_DIR}/wheelhouse
   svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0/test_wheels
-  cd test_wheels
-  for artifact in ../wheelhouse/*.*; do
+  for artifact in *.*; do
     echo $artifact
+    cd 2.4.0/test_wheels
     svn add $artifact
+    cd ../..
   done
   svn commit --non-interactive --no-auth-cache --username $WHEELHOUSE_UPLOADER_USERNAME --password $WHEELHOUSE_UPLOADER_SECRET -m "Upload python wheels"
 }
