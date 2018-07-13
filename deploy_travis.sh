2139fbeb5c4045e9d224fa5e1137ea5d6d3e23d1
Boyuan Zhang <boyuanz@google.com>
2018-07-13 16:16:27 -0700

    debug svn add

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -3,10 +3,11 @@
 function deploy {
   cd ${TRAVIS_BUILD_DIR}/wheelhouse
   mkdir test_wheels
-  svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0/test_wheels
+  svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0
   for artifact in *.*; do
     echo $artifact
     mv $artifact test_wheels/$artifact
+    ls -lh test_wheels
     svn add test_wheels/$artifact
   done
   svn commit --non-interactive --no-auth-cache --username $WHEELHOUSE_UPLOADER_USERNAME --password $WHEELHOUSE_UPLOADER_SECRET -m "Upload python wheels"
