14c028bba44f43713b997b83019d91eb0f74e76d
Boyuan Zhang <boyuanz@google.com>
2018-07-13 16:29:17 -0700

    debug svn add retry #3

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,10 +1,9 @@
 # Define custom utilities for deploying on travis
 
 function deploy {
-  cd ${TRAVIS_BUILD_DIR}/wheelhouse
-  mkdir test_wheels
+  cd ${TRAVIS_BUILD_DIR}
   svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0
-  for artifact in *.*; do
+  for artifact in wheelhouse/*.*; do
     echo $artifact
     mv $artifact 2.4.0/test_wheels/$artifact
     ls -lh 2.4.0/test_wheels
