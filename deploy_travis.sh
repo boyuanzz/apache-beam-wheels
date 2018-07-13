a14b3f1f0568bc901d1646eb3a512fa4916a9e1f
Boyuan Zhang <boyuanz@google.com>
2018-07-13 16:39:59 -0700

    debug svn add retry #4

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -3,11 +3,12 @@
 function deploy {
   cd ${TRAVIS_BUILD_DIR}
   svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0
-  for artifact in wheelhouse/*.*; do
+  cd wheelhouse
+  for artifact in *.*; do
     echo $artifact
-    mv $artifact 2.4.0/test_wheels/$artifact
-    ls -lh 2.4.0/test_wheels
-    svn add 2.4.0/test_wheels/$artifact
+    mv $artifact ../2.4.0/test_wheels/$artifact
+    ls -lh ../2.4.0/test_wheels
+    svn add ../2.4.0/test_wheels/$artifact
   done
   svn commit --non-interactive --no-auth-cache --username $WHEELHOUSE_UPLOADER_USERNAME --password $WHEELHOUSE_UPLOADER_SECRET -m "Upload python wheels"
 }
