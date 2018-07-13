8529916431e3d9161f6153bc753eeec214190ad5
Boyuan Zhang <boyuanz@google.com>
2018-07-13 15:32:23 -0700

    add $ to var

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,7 +1,7 @@
 # Define custom utilities for deploying on travis
 
 function deploy {
-  cd {TRAVIS_BUILD_DIR}/wheelhouse
+  cd ${TRAVIS_BUILD_DIR}/wheelhouse
   svn co https://dist.apache.org/repos/dist/dev/beam/2.4.0/test_wheels
   for artifact in *.*; do
     echo $artifact
